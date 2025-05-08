f118b4c74103bc006bd33c37d59f3ba56fe05501
Mohammad Saheeb Masood <saheebmasood@gmail.com>
2025-05-09 01:09:12 +0530

    Update playlist.js

--- playlist.js
+++ playlist.js
@@ -1,13 +1,29 @@
 // playlist.js
 
 const songs = [
-    { title: ' ', artist: ' ', file: '', albumArt: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSj2Yf9_3cFkT6lXJ16UzzzErQ_Ho1yheAbEA&s' },
-    { title: 'Sorry', artist: 'Justin Bieber', file: 'https://themamaship.com/music/Catalog/Sorry%20-%20Justin%20Bieber.mp3', albumArt: 'https://upload.wikimedia.org/wikipedia/en/d/dc/Justin_Bieber_-_Sorry_%28Official_Single_Cover%29.png' },
-    { title: 'Perfect', artist: 'Ed Sheeran', file: 'https://themamaship.com/music/Catalog/Perfect%20-%20Ed%20Sheeran.mp3', albumArt: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRT3_DmF-D7Nfee1Iu5Z_ftUYlW03gOsb_ZMQ&s' },
-    { title: 'Perfect', artist: 'Ed Sheeran', file: 'https://rb.gy/e11s9o', albumArt: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRT3_DmF-D7Nfee1Iu5Z_ftUYlW03gOsb_ZMQ&s' },
-    { title: 'Nadaaniyan', artist: 'Akshath', file: 'https://sambalpuristar.in/files/download?id=7197', albumArt: 'https://c.saavncdn.com/722/nadaaniyan-Hindi-2024-20240517212238-500x500.jpg' },
-    // ... add other songs here
-];
+        { title: ' ', artist: ' ', file: '', albumArt: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSj2Yf9_3cFkT6lXJ16UzzzErQ_Ho1yheAbEA&s' },
+        { title: 'Sorry', artist: 'Justin Bieber', file: 'https://themamaship.com/music/Catalog/Sorry%20-%20Justin%20Bieber.mp3', albumArt: 'https://upload.wikimedia.org/wikipedia/en/d/dc/Justin_Bieber_-_Sorry_%28Official_Single_Cover%29.png' },
+        { title: 'Perfect', artist: 'Ed Sheeran', file: 'https://themamaship.com/music/Catalog/Perfect%20-%20Ed%20Sheeran.mp3', albumArt: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRT3_DmF-D7Nfee1Iu5Z_ftUYlW03gOsb_ZMQ&s' },
+        { title: 'Perfect', artist: 'Ed Sheeran', file: 'https://rb.gy/e11s9o', albumArt: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRT3_DmF-D7Nfee1Iu5Z_ftUYlW03gOsb_ZMQ&s' },
+        { title: 'Nadaaniyan', artist: 'Akshath', file: 'https://sambalpuristar.in/files/download?id=7197', albumArt: 'https://c.saavncdn.com/722/nadaaniyan-Hindi-2024-20240517212238-500x500.jpg' },
+        { title: 'Sadqay', artist: 'Aashir Wajahat, Nayel, Nehaal Naseem', file: 'https://aac.saavncdn.com/290/b7855d9aa06cfd886e196b1008f79e72_160.mp4', albumArt: 'https://pagalsongs.com.in/siteuploads/thumb/sft25/12188_7.webp' },
+        { title: 'Suniyan Suniyan', artist: 'Juss', file: 'https://raagjatt.dev/load/1gkdpnz51vxkq894z7me38dnq8njpyrn5b2l6dg4qebyl/Suniyan Suniyan (RaagJatt.COM).mp3', albumArt: 'https://raagjatt.dev/images/song/lmodkmdj/suniyan-suniyan-juss.webp' },
+        { title: 'Tere Hawaale', artist: 'Pritam, Arijit Singh, Shilpa Rao', file: 'https://aac.saavncdn.com/576/b34009b2f8262c379ac410d8e32f7206_160.mp4', albumArt: 'https://c.saavncdn.com/576/Laal-Singh-Chaddha-Hindi-2022-20220805121002-500x500.jpg' },
+        { title: 'Maan Meri Jaan', artist: 'King', file: 'https://aac.saavncdn.com/734/31a101fae38e184208e5f95e3e6c756d_160.mp4', albumArt: 'https://lh3.googleusercontent.com/B8qfmAm8BOtXmSEwYMkcotK6nun-imczlC1r8OyeCxGMJqqfoxFkqCWL38MgFN3dcmYp63PLo4aQYbo=w544-h544-l90-rj' },
+        { title: 'Dil Tu Jaan Tu', artist: 'Gurnazar', file: 'https://s320.djpunjab.is/data/48/55677/304732/Dil%20Tu%20Jaan%20Tu%20-%20Gurnazar.mp3', albumArt: 'https://lh3.googleusercontent.com/y_6HpyQI58bOeOHo1E9umBPfiqbe3iWMHv3_2dKlwVsyRcaUhIXAHnneTXkBBs14BrCJPR2kp1pwWRNO_A=w544-h544-l90-rj' },
+        { title: 'Husn', artist: 'Husn  by Anuv Jain', file: 'https://aac.saavncdn.com/436/13795b7aa2e87393366162b9e6a6fe88_160.mp4', albumArt: 'https://c.saavncdn.com/436/Husn-Hindi-2023-20231129054140-500x500.jpg' },
+        { title: 'Shikayat', artist: 'AUR', file: 'https://aac.saavncdn.com/433/5d0773379e72eb85562c91b3b193d4e0_160.mp4', albumArt: 'https://c.saavncdn.com/433/Shikayat-Hindi-2023-20231214181447-500x500.jpg' },
+        { title: 'Mehrama', artist: 'Pritam, Darshan Raval, Antara Mitra', file: 'https://aac.saavncdn.com/862/3a36773fb96a2f1232ba4463c6fb5a91_160.mp4', albumArt: 'https://c.saavncdn.com/862/Love-Aaj-Kal-Hindi-2020-20200214140423-500x500.jpg' },
+        { title: 'Pehli Si Muhabbat', artist: 'Ali Zafar', file: 'https://aac.saavncdn.com/430/7d45acf118733488bbdcdfc931533d27_160.mp4', albumArt: 'https://c.saavncdn.com/430/Pehli-Si-Muhabbat-English-2021-20210612154544-500x500.jpg' },
+        { title: 'Pal', artist: ' Javed-Mohsin, Samuel & Akanksha, Tanishk Bagchi', file: 'https://aac.saavncdn.com/619/14531944c089c3c9c318e0b73b4b8ee8_160.mp4', albumArt: 'https://c.saavncdn.com/619/Jalebi-Hindi-2018-20180921123431-500x500.jpg' },
+        { title: 'Dance Monkey', artist: 'Tones and I', file: 'https://themamaship.com/music/Catalog/Dance%20Monkey%20-%20Tones%20and%20I.mp3', albumArt: 'https://i.scdn.co/image/ab67616d0000b2732259742879137c15e1a0fa1f' },
+        { title: 'Abhi Abhi', artist: 'KK', file: 'https://aac.saavncdn.com/198/8b68fbba0c3c0ea2ce0c01e82b2af966_160.mp4', albumArt: 'https://c.saavncdn.com/198/Jism-2-Hindi-2012-20211217161016-500x500.jpg' },
+        { title: 'Chal Diye Tum Kahan', artist: 'Ahad Khan, Usama Ali', file: 'https://aac.saavncdn.com/744/2279c4c2bd2fc15d10bb473f7ad7728f_160.mp4', albumArt: 'https://c.saavncdn.com/744/Kabhi-Main-Kabhi-Tum-Original-Motion-Picture-Soundtrack-Urdu-2024-20240615153212-500x500.jpg' },
+        { title: 'O Maahi', artist: 'Pritam, Arijit Singh, Irshad Kamil', file: 'https://aac.saavncdn.com/139/61036495c7ba45adf72a856b60f054fd_160.mp4', albumArt: 'https://c.saavncdn.com/139/Dunki-Hindi-2023-20231220211003-500x500.jpg' },
+        { title: 'Samjhawan', artist: 'Jawad Ahmed, Sharib Toshi, Arijit Singh, Shreya Ghoshal', file: 'https://aac.saavncdn.com/540/f807aad8e5c60a87334231f72267c725_160.mp4', albumArt: 'https://c.saavncdn.com/540/Humpty-Sharma-Ki-Dulhania-Hindi-2014-20190618095042-500x500.jpg' },
+        
+        
+    ];
 
 let currentIndex = Math.floor(Math.random() * 11);
 
